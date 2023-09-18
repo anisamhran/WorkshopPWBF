1dfa9c2eb590a65437e4b66e88e48b629cc69829
daniruliansyah <daniruliansyah03@sma.belajar.id>
2023-09-18 18:02:54 +0700

    DATABASE + RELASI

---
+++ database/migrations/2023_09_18_080925_create_pekerja_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pekerja', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pekerja');
+            $table->date('tgl_lahir');
+            $table->string('alamat_pekerja');
+            $table->string('no_hp_ewallet', 13);
+            $table->binary('foto_pekerja');
+            $table->binary('ktp_pekerja');
+            $table->text('deskripsi_pekerja');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pekerja');
+    }
+};
